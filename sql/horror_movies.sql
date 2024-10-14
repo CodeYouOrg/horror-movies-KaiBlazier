73ccafb2d1e0a3b08169d7a838eacc2f3d269793
KaiBlazier <kaiblazier16@gmail.com>
2024-10-13 19:16:04 -0500

    Update horror_movies.sql
    I think I did this correctly will share screen on Wednesday with class.

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,10 @@
--- Add your SQL here
+Select
+id AS Movie_ID, name AS Movie_Title, imdb_rating AS Rating
+From
+movies
+WHERE
+genre = 'Horror'
+AND release_year <= 1985
+ORDER BY
+imdb_rating DESC
+Limit 3;
